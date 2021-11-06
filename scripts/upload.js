778e9043f3666075fb9e9d2fa8bb3381613ec040
Brian Nhieu <bnhieu@ucsd.edu>
2021-11-05 17:13:15 -0700

    fixed logo

--- scripts/upload.js
+++ scripts/upload.js
@@ -2,17 +2,15 @@
 
 function init() {
 
-
 }
 
-let upload = document.getElementById('upload');
-console.log(upload);
-if(upload){
-    upload.addEventListener('click', (event) => {
-        var userImg = document.getElementById('user');
-        /* Need to figure out what eventListener to use, not saving the final img  ^^ load does not work... */
-        localStorage.setItem('userImg', userImg);
 
+let upload = document.getElementById('upload');
+if(upload){
+    /* Unsure how to correctly set/use localStorage */
+    upload.addEventListener('change', (event) => {
+        let userImg = document.getElementById('user');
+        localStorage.setItem('userImg', userImg);
     })
 }
 
